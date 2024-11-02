fcf5a77c48071dc7f077647594a89adfab4b806c
MSI <hirun08072002@gmail.com>
2024-11-02 18:06:42 +0530

    signup UI

--- src/bank/management/system/Signup.java
+++ src/bank/management/system/Signup.java
@@ -5,8 +5,10 @@ import javax.swing.*;
 public class Signup extends JFrame {
 
     Signup(){
+        setLayout(Null);
         setSize(850,800);
         setLocation(360,40);
+        setVisible(true);
     }
     public static void main(String[] args) {
 new Signup();

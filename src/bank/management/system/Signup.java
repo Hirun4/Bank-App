e32a6536a0520c691acbd8a5623f77bb75ab196a
MSI <hirun08072002@gmail.com>
2024-11-02 18:32:19 +0530

    signup UI

--- src/bank/management/system/Signup.java
+++ src/bank/management/system/Signup.java
@@ -6,6 +6,7 @@ import java.awt.*;
 public class Signup extends JFrame {
 
     Signup(){
+        super("APPLICATION FORM");
         getContentPane().setBackground(new Color(222,255,228));
         setLayout(null);
         setSize(850,800);

b925c8f7b25c3c2ec2e347bc7cd68eb957aa90ab
MSI <hirun08072002@gmail.com>
2024-11-02 18:35:17 +0530

    signup UI update

--- src/bank/management/system/Signup.java
+++ src/bank/management/system/Signup.java
@@ -11,7 +11,7 @@ public class Signup extends JFrame {
         Image i2 = i1.getImage().getScaledInstance(100,100,Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel image = new JLabel(i3);
-        image.setBounds(350,10,100,100);
+        image.setBounds(25,10,100,100);
         add(image);
 
 

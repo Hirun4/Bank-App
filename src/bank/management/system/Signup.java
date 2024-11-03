f520da9d7cf01fabfc4c1ae5bc7477222e044211
MSI <hirun08072002@gmail.com>
2024-11-03 18:21:02 +0530

    pin code

--- src/bank/management/system/Signup.java
+++ src/bank/management/system/Signup.java
@@ -8,7 +8,7 @@ import java.util.Random;
 
 public class Signup extends JFrame {
     JRadioButton r1,r2;
-    JTextField textName,textFname,textEmail,textMs,textAdd,textCity;
+    JTextField textName,textFname,textEmail,textMs,textAdd,textCity,textPin;
     JDateChooser dateChooser;
 
 

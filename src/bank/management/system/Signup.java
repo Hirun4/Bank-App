092218041db7fc13b1064d978afd42140315b451
MSI <hirun08072002@gmail.com>
2024-11-03 17:37:56 +0530

    email

--- src/bank/management/system/Signup.java
+++ src/bank/management/system/Signup.java
@@ -102,7 +102,7 @@ public class Signup extends JFrame {
         textEmail = new JTextField();
         textEmail.setFont(new Font("Raleway",Font.BOLD,14));
         textEmail.setBounds(300,240,400,30);
-        add(textFname);
+        add(textEmail);
 
 
 

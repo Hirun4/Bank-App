f2b613f1a111f48fa45a43fa1045cc90fa8c2692
MSI <hirun08072002@gmail.com>
2024-11-03 18:34:53 +0530

    Married radio button

--- src/bank/management/system/Signup.java
+++ src/bank/management/system/Signup.java
@@ -119,7 +119,7 @@ public class Signup extends JFrame {
         m2.setFont(new Font("Raleway",Font.BOLD,14));
         add(m2);
 
-        m3 = new JRadioButton("unmarried");
+        m3 = new JRadioButton("Other");
         m3.setBounds(300,440,100,30);
         m3.setFont(new Font("Raleway",Font.BOLD,14));
         add(m3);

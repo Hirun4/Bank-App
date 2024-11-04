89cd819fa7329ad0352442037da5d0b9dba723d4
MSI <hirun08072002@gmail.com>
2024-11-04 13:29:00 +0530

    setting signup backend

--- src/bank/management/system/newCon.java
+++ src/bank/management/system/newCon.java
@@ -14,7 +14,7 @@ public class newCon {
     public newCon(){
 
         try{
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem","root", "Hirun500#%");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem","root", "12345678");
             statement = connection.createStatement();
 
         }catch(Exception e){

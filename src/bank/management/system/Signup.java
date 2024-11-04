28d0a31d1dc4e0863546dcc269cf816190792ed5
MSI <hirun08072002@gmail.com>
2024-11-04 15:01:12 +0530

    setting signup backend

--- src/bank/management/system/Signup.java
+++ src/bank/management/system/Signup.java
@@ -224,6 +224,7 @@ public class Signup extends JFrame implements ActionListener {
         String  address = textAdd.getText();
         String  city = textCity.getText();
         String pincode = textPin.getText();
+        String state = textState.getText();
 
             
 

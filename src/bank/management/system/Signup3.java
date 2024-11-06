56b3482bd0a8dee220e9a8da47e51c53f2f5e224
MSI <hirun08072002@gmail.com>
2024-11-06 09:49:40 +0530

    signup 2 and 3 completed

--- src/bank/management/system/Signup3.java
+++ src/bank/management/system/Signup3.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+//signup 3
 public class Signup3 extends JFrame implements ActionListener {
 
     JRadioButton r1,r2,r3,r4;

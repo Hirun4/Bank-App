e3fb01ebb9975f8a7d686f521f68a56f94f431d4
MSI <hirun08072002@gmail.com>
2024-11-07 14:34:19 +0530

    signup 2 action setupping

--- src/bank/management/system/Signup2.java
+++ src/bank/management/system/Signup2.java
@@ -211,7 +211,7 @@ public class Signup2 extends JFrame implements ActionListener {
 
             }else{
                 newCon c1 = new newCon();
-                String q= "insert into Signuptwo values('"+formno+"')";
+                String q= "insert into Signuptwo values('"+formno+"','"+rel+"')";
             }
 
         }catch(Exception E){

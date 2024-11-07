7f2fedb8737a699b2cf5207242601ed875c031e6
MSI <hirun08072002@gmail.com>
2024-11-07 19:49:31 +0530

    signup three setting up

--- src/bank/management/system/Signup3.java
+++ src/bank/management/system/Signup3.java
@@ -211,7 +211,7 @@ public class Signup3 extends JFrame implements ActionListener {
         } else if (c2.isSelected()) {
             fac = fac + "Internet Banking";
         } else if (c3.isSelected()) {
-            fac = fac + "";
+            fac = fac + "Mobile Banking";
         }
 
 

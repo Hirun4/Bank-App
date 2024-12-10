3a611c84aa8dfb056a7685b0643e18f9a9008dbf
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-10 18:24:27 +0530

    mini

--- src/bank/management/system/mini.java
+++ src/bank/management/system/mini.java
@@ -1,10 +1,13 @@
 package bank.management.system;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class mini extends JFrame {
 
     mini(){
+
+        getContentPane().setBackground(new Color(255,204,204));
         setSize(400,600);
         setLocation(20,20);
         setVisible(true);
